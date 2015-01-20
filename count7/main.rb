0f82bf465be85c9a7a790517e02dcb4b074ab444
inu <inu.lnlog@gmail.com>
2015-01-20 20:55:04 +0900

    計測はBenchmarkを使ってみる

--- count7/main.rb
+++ count7/main.rb
@@ -5,13 +5,15 @@ require './count7'
 
 counter7 = Count7.new(7)
 
-tStart = Time.now.usec
-counter7.count(7)
-counter7.count(77)
-counter7.count(99)
-counter7.count(77777)
-counter7.count(23678947)
-counter7.count(732465890)
-counter7.count(1912478363)
-tEnd = Time.now.usec - tStart
-puts "time : #{tEnd} usec"
+proc_time = Benchmark.realtime do
+  #counter7.count(7)
+  counter7.count(70)
+  #counter7.count(77)
+  counter7.count(700)
+  counter7.count(99)
+  counter7.count(77777)
+  counter7.count(23678947)
+  counter7.count(732465890)
+  counter7.count(1912478363)
+end
+puts "time : #{proc_time} sec"
